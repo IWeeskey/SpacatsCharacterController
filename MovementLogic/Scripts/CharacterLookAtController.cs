929e006d87b6ff7a56e216005d1017837da4b837
IvanKurdakov <kis.flack.more.more@gmail.com>
2026-05-03 15:52:07 +0300

    - working look at logic

--- MovementLogic/Scripts/CharacterLookAtController.cs
+++ MovementLogic/Scripts/CharacterLookAtController.cs
@@ -9,101 +9,43 @@ namespace Spacats.CharacterController
     public class CharacterLookAtController : MonoBehaviour
     {
         public bool DoLogic = true;
-        public float Speed = 10f;
+        [SerializeField] private float _stopRotateSpeed;
+        [SerializeField] private Vector2 _maxAngle;
+        private float _currentRotateMultiplier;
+        private float _targetRotateMultiplier;
+        private float _angle;
         [SerializeField] private Animator _animator;
         [SerializeField] private List<float> _weights = new List<float>();
-        [SerializeField] private List<Transform> _bones = new List<Transform>();
-        private List<Transform> _boneRefs = new List<Transform>();
-        [SerializeField] private List<float> _bonesWeights = new List<float>();
-        
-        [SerializeField] private List<Quaternion> _bonesQuats = new List<Quaternion>();
         
         private CharacterInputRuntimeData _inputData;
         
         public void Init(CharacterInputRuntimeData inputData)
         {
             _inputData = inputData;
-            
-            for (int i = 0; i < _bones.Count; i++)
-            {
-                _bonesQuats.Add(_bones[i].rotation);
-                GameObject newGO = new GameObject();
-                newGO.name = _bones[i].name;
-                newGO.transform.SetParent(gameObject.transform);
-                _boneRefs.Add( newGO.transform);
-                
-                
-            }
         }
 
         private void OnAnimatorIK(int layerIndex)
         {
             if (!DoLogic) return;
+            if (PauseController.IsPaused) return;
+            
+            RefreshRotateMultiplier();
+            float finalWeight = _weights[0] * _currentRotateMultiplier;
+            if (finalWeight<0) return;
+            
             _animator.SetLookAtPosition(_inputData.LookAtPoint);
-            _animator.SetLookAtWeight(_weights[0], _weights[1], _weights[2], _weights[3], _weights[4]);
-
-            for (int i = 0; i < _bones.Count; i++)
-            {
-                Transform bone = _bones[i];
-                
-                
-                //_bonesQuats[i] = bone.rotation;
-                //bone.LookAt(_inputData.LookAtPoint);
-            }
+            _animator.SetLookAtWeight(finalWeight, _weights[1], _weights[2], _weights[3], _weights[4]);
         }
 
-        private void FixedUpdate()
+        private void RefreshRotateMultiplier()
         {
-            //_animator.Update(0);
-            for (int i = 0; i < _bones.Count; i++)
-            {
-                Transform bone = _bones[i];
-                _bonesQuats[i] = bone.rotation;
-                //bone.LookAt(_inputData.LookAtPoint);
-                // Transform refBone = _boneRefs[i];
-                // refBone.position = bone.position;
-                // refBone.LookAt(_inputData.LookAtPoint);
-            }
-        }
-
-        // private void LateUpdate()
-        // {
-        //     for (int i = 0; i < _bones.Count; i++)
-        //     {
-        //         Transform bone = _bones[i];
-        //         Quaternion beforeQuat = _bonesQuats[i];
-        //         bone.LookAt(_inputData.LookAtPoint);
-        //         Quaternion targetQuat = bone.rotation;
-        //         
-        //         bone.rotation = Quaternion.Lerp(beforeQuat, targetQuat, Time.deltaTime * Speed);
-        //     }
-        // }
-
-        public void LateUpdate()
-        {
-            //_animator.Update(0);
-            return;
-            for (int i = 0; i < _bones.Count; i++)
-            {
-                Transform bone = _bones[i];
-                
-                /*Transform refBone = _boneRefs[i];
-                refBone.position = bone.position;
-                refBone.LookAt(_inputData.LookAtPoint);*/
-                
-                Transform refBone = _boneRefs[i];
-                refBone.position = bone.position;
-                refBone.LookAt(_inputData.LookAtPoint);
-                
-                Quaternion targetQuat = refBone.rotation;
-                
-                Quaternion beforeQuat = _bonesQuats[i];
-                //beforeQuat = bone.rotation;
-                bone.LookAt(_inputData.LookAtPoint);
-                targetQuat = bone.rotation;
-                
-                bone.rotation = Quaternion.Lerp(beforeQuat, targetQuat, Time.deltaTime * Speed);
-            }
+            Vector3 direction = (-gameObject.transform.position +_inputData.LookAtPoint).normalized;
+            _angle = Vector3.SignedAngle(gameObject.transform.forward, direction, Vector3.up);
+            _targetRotateMultiplier = 1f;
+            if (_angle > 0 && _angle > _maxAngle.y) _targetRotateMultiplier = 0f;
+            if (_angle < 0 && _angle < _maxAngle.x) _targetRotateMultiplier = 0f;
+            
+            _currentRotateMultiplier = Mathf.Lerp(_currentRotateMultiplier, _targetRotateMultiplier, Time.deltaTime * _stopRotateSpeed);
         }
     }
 }
